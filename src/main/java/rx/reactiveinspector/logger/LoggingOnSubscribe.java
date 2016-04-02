06922eb78e269a4527b4c200650a6e6d12f392a0
Kai Engelhardt <kai.engelhardt93@gmail.com>
2016-04-02 21:35:44 +0200

    Add global registry for Observables

--- src/main/java/rx/reactiveinspector/logger/LoggingOnSubscribe.java
+++ src/main/java/rx/reactiveinspector/logger/LoggingOnSubscribe.java
@@ -1,16 +1,23 @@
 package rx.reactiveinspector.logger;
 
+import java.util.Collections;
+import java.util.Map;
 import java.util.UUID;
+import java.util.WeakHashMap;
 
 import rx.Observable.OnSubscribe;
 import rx.Subscriber;
 
 public class LoggingOnSubscribe<T> implements OnSubscribe<T>, Debuggable<T> {
 	
+	@SuppressWarnings("rawtypes") // In this case a raw type is desirable
+	private static Map<OnSubscribe, UUID> registryTable = Collections.synchronizedMap(new WeakHashMap<>());
+	
 	private OnSubscribe<T> onSubscribe;
 		
 	public LoggingOnSubscribe(OnSubscribe<T> onSubscribe) {
 		this.onSubscribe = onSubscribe;
+		registryTable.put(onSubscribe, debugID);
 		RxLogger.getSharedLogger().logNodeCreated(this);
 	}
 	
@@ -26,6 +33,10 @@ public class LoggingOnSubscribe<T> implements OnSubscribe<T>, Debuggable<T> {
 	@Override
 	public UUID getDebugID() {
 		return debugID;
+	}
+	
+	public static <T> UUID getDebugIDForOnSubscribe(OnSubscribe<T> onSubscribe) {
+		return registryTable.get(onSubscribe);
 	}
 	
 	// MARK: - Private API
